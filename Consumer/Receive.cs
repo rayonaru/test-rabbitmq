295b36d5409951190cb5a0efe6bad8a357cf9aa4
rayonaru <vova.tuktanov@mail.ru>
2021-10-08 16:38:25 +0300

    init

---
+++ Consumer/Receive.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() 
+            { 
+                HostName = "localhost", 
+                UserName = "admin",
+                Password = "admin"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "raru",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x] Received {message}");
+                    };
+
+                    channel.BasicConsume
+                    (
+                        queue: "raru",
+                        autoAck: true,
+                        consumer: consumer
+                    );
+
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Send.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() 
+            { 
+                HostName = "localhost", 
+                UserName = "admin",
+                Password = "admin"
+            };
+            
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "raru",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var message = "Hello World!";
+                    
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "raru",
+                        basicProperties: null,
+                        body: body
+                    );
+
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
